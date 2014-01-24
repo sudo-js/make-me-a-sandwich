1e853465612949b50895eadf0de1f5242f40b077
robrobbins <rob.robb.ns@gmail.com>
2014-01-23 18:02:52 -0800

    update docs and specs

--- extras/addOns/nodeList.js
+++ extras/addOns/nodeList.js
@@ -1,14 +1,34 @@
-// ###forEach
+// ###forEach (NodeList)
 // This is a, hopefully, shortlived bit of syntactic sugar for the fact that,
-// pre-ES6, there isn't a succinct way to iterate over a NodeList. This method
-// will simply construct a for loop around the passed in NodeList and call the
-// provided function with each item in it
+// at the moment, there isn't a succinct way to iterate over a NodeList such as `Array.forEach`. 
+// This method will simply construct a `for` loop around the passed in NodeList and call the
+// provided function with each item in it, returning the list
 //
 // `param` {nodeList} `list`
-// `param` {function} `fn` 
+// `param` {function} `fn`
+// `returns` {nodeList} `list` 
 NodeList.forEach = function nlforEach(list, fn) {
   var len = list.length, i;
   for (i = 0; i < len; i++) {
     fn(list[i]);
   }
+  return list;
+};
+// ###filter (NodeList)
+// This is a, hopefully, shortlived bit of syntactic sugar for the fact that,
+// at the moment, there isn't an equivalent way to filter a NodeList the same as 
+// an Array. This method will simply construct a for loop around the passed in NodeList and call the
+// provided function with each item in it, appending the items that pass a truthy test to a 
+// new nodeList and returning that
+//
+// `param` {nodeList} `list`
+// `param` {function} `fn` 
+// `returns` {nodeList}  
+NodeList.filter = function nlfilter(list, fn) {
+  var len = list.length, frag = document.createDocumentFragment(), i;
+  for (i = 0; i < len; i++) {
+    // there is no accessible constructor for a nodeList, so use childNodes
+    if(fn(list[i])) frag.appendChild(list[i]);
+  }
+  return frag.childNodes;
 };
--- extras/addOns/spec/addOns.spec.js
+++ extras/addOns/spec/addOns.spec.js
@@ -7,6 +7,33 @@ describe('sudo native class add-ons', function() {
     expect(Element.matches(div.lastChild, '[type="button"]')).toBe(true);
   });
   
+  it('finds the closest parent matching a selector', function() {
+    var div = document.createElement('div');
+    div.innerHTML = '<section><span class="foo"></span><input type="button"></input></section>';
+    expect(Node.closestParent(div.lastChild, 'div')).toEqual(div);
+  });
+  
+  it('can use NodeList.forEach', function() {
+    var div = document.createElement('div'), spn1, spn2;
+    div.innerHTML = '<section><span></span><input type="button"></input><span></span><input type="button"></input></section>';
+    NodeList.forEach(div.querySelectorAll('span'), function(spn) {
+      spn.classList.add('foo');
+    });
+    spn1 = div.childNodes[0].childNodes[0];
+    expect(spn1.classList.contains('foo')).toBe(true);
+    spn2 = div.childNodes[0].childNodes[2];
+    expect(spn2.classList.contains('foo')).toBe(true);
+  });
+  
+  it('can use NodeList.filter', function() {
+    var div = document.createElement('div'), filtered;
+    div.innerHTML = '<section><span></span><a href="#" data-foo="foo"></a><span></span><a href="#"></input></section>';
+    filtered = NodeList.filter(div.querySelectorAll('a'), function(spn) {
+      return !spn.getAttribute('data-foo');
+    });
+    expect(filtered.length).toBe(1);
+  });
+  
   it('serializes a hash into a "params" type string', function() {
     expect(Object.serialize({foo: 'bar', baz: 'qux'})).toEqual("foo=bar&baz=qux");
   });
