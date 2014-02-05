3c3959ca31d42207bd56f0175b48ca58e131ef91
robrobbins <rob.robb.ns@gmail.com>
2014-02-04 16:18:32 -0800

    cleaning up

--- extras/delegates/infiniteScroll/infiniteScroll.js
+++ extras/delegates/infiniteScroll/infiniteScroll.js
@@ -1,3 +1,4 @@
+// Be sure to get the Function.debounce method from Static.js if using this.
 sudo.delegates.infiniteScroll = function(opts) {
   opts || (opts = {});
   // how far from the bottom until calling scroll action?
@@ -6,19 +7,27 @@ sudo.delegates.infiniteScroll = function(opts) {
   this.inverted = !!opts.inverted;
   // if only a certain Element is to be scrolled, not the default 'window'
   // can be a string selector (assumed to be in the scope of the delegator)
-  // or a $wrapped object
-  // must be normalized later in the addedAsDelegate method
-  this.$scrollable = opts.scrollable || $(window);
-  // the element that contains the scrollable, defalts to the outer wrapper,
-  // same rules as the scrollable
-  this.$container = opts.container || $('.main-content');
+  // normalized further in the addedAsDelegate method. In a mobile scenario
+  // this shourd be falsey
+  this.scrollable = opts.scrollable || window;
+  // the element that contains the scrollable, defalts to 'body'
+  this.container = opts.container || document.querySelector('body');
+  // we must retain a ref to the bound function so we can remove it
+  this._boundScroll = this.scrolled.bind(this);
+  // normalize the correct operations for measuring height
 };
 
 sudo.delegates.infiniteScroll.prototype = {
   addedAsDelegate: function addedAsDelegate(delegator) {
     // normalize the passed in scrollable and container if present
-    if(typeof this.$scrollable === 'string') this.$scrollable = delegator.$(this.$scrollable);
-    if(typeof this.$container === 'string') this.$container = delegator.$(this.$container);
+    if(typeof this.scrollable === 'string') this.scrollable = delegator.qs(this.scrollable);
+    if(typeof this.container === 'string') this.container = delegator.qs(this.container);
+    // it's important to know when scrollable is window
+    this.scrollableIsWindow = this.scrollable === window;
+    // depending on what the scrollable is, set the proper height attr
+    this.scrollTopAttr = this.scrollableIsWindow ? 'pageYOffset' : 'scrollTop';
+    // if its the document, assure its the documentElement
+    if(Node.isDocument(this.scrollable)) this.scrollable = this.scrollable.documentElement;
     // in the inverted case, do not setup until called. this is because unless the 
     // scrollable has been moved to the bottom, any scroll would trigger the call to infiniteScrollAction
     if(!this.inverted) this.setUp();
@@ -26,27 +35,30 @@ sudo.delegates.infiniteScroll.prototype = {
   // all delegates require a role which the delegator can use 
   // when using this.delagate(<role>)...
   role: 'infiniteScroll',
-  scrolled: function scrolled(ev) {
-    // common to either inverted or not
-    var fn = function() {
-      this.$scrollable.off('scroll');
-      this.isListening = false;
-      if('infiniteScrollAction' in this.delegator) {
-        // pass my setup so the delegator can rebind if desired
-        this.delegator.infiniteScrollAction(this.setUp.bind(this));
-      }
-    }.bind(this);
-
+  // common to either the inverted case or non-inverted 
+  scroll: Function.debounce(function(e) {
+    this.scrollable.removeEventListener('scroll', this._boundScroll);
+    this.isListening = false;
+    if('infiniteScrollAction' in this.delegator) {
+      // pass my setup so the delegator can rebind if desired
+      this.delegator.infiniteScrollAction(this.setUp.bind(this));
+    }
+  }, 300, true),
+  // ---
+  scrolled: function scrolled(e) {
+    var scrollableHeight = this.scrollableIsWindow ? this.scrollable.innerHeight :
+      Node.getHeight(this.scrollable);
     if(this.inverted) {
-      if(this.$scrollable.scrollTop() <= this.trigger) fn();
+      if(this.scrollable[this.scrollTopAttr] <= this.trigger) this.scroll();
     } else {
-      if(this.$scrollable.scrollTop() + this.trigger >= (this.$container.height() - this.$scrollable.height())) fn();
+      if(this.scrollable[this.scrollTopAttr] + this.trigger >= 
+        (Node.getHeight(this.container) - scrollableHeight)) this.scroll();
     }
   },
   // sets up the scroll listening
   setUp: function setUp() {
     if(!this.isListening) {
-      this.$scrollable.on('scroll', this.scrolled.bind(this));
+      this.scrollable.addEventListener('scroll', this._boundScroll);
       this.isListening = true;
     }
     return this;
@@ -54,17 +66,28 @@ sudo.delegates.infiniteScroll.prototype = {
   // when being unloaded the delegator should
   // make sure the scroll listener is off
   tearDown: function tearDown() {
-    this.$scrollable.off('scroll');
+    this.scrollable.removeEventListener('scroll', this._boundScroll);
     return this;
   },
   // force the scroll to the bottom of the scrollable
   toBottom: function toBottom() {
-    this.$scrollable[0].scrollTop = this.$scrollable[0].scrollHeight;
+    var scr;
+    // if the scrollable is window (or document) you are gonna have to set the scrolltop on body as
+    // setting the pageYOffset || scrollTop will not have the desired effect
+    if(this.scrollableIsWindow || Node.isDocument(this.scrollable)) {
+      scr = document.querySelector('body');
+      scr.scrollTop = scr.scrollHeight;
+    } else this.scrollable.scrollTop = this.scrollable.scrollHeight;
     return this;
   },
   // force the scroll to the top of the scrollable
   toTop: function toTop() {
-    this.$scrollable[0].scrollTop = 0;
+    var scr;
+    // same as toBottom...
+    if(this.scrollableIsWindow || Node.isDocument(this.scrollable)) {
+      scr = document.querySelector('body');
+      scr.scrollTop = 0;
+    } else this.scrollable.scrollTop = 0;
     return this;
   }
 };
