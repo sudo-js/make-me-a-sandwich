6d9b5f219a265f050eb3a7f120c0e037767a67f7
robrobbins <rob.robb.ns@gmail.com>
2014-04-03 13:02:54 -0700

    DataView out of the default build, data delegate as well

--- build/debug/sudo.js
+++ build/debug/sudo.js
@@ -675,86 +675,6 @@ sudo.template = function template(str, data, scope) {
   tmpl.source = 'function(' + scope + '){\n' + source + '}';
   return tmpl;
 };
-// ##DataView Class Object
-
-// Create an instance of an Object, inheriting from sudo.View that:
-// 1. Expects to have a template located in its internal data Store accessible via `this.data.template`.
-// 2. Can have a `renderTarget` property in its data store. If so this will be the location
-//		the child injects itself into (if not already in) the DOM
-// 3. Can have a 'renderMethod' property in its data store. If so this is the DOM method
-//		that the child will use to place itself in it's `renderTarget`.
-// 4. Has a `render` method that when called re-hydrates it's el by passing a
-//		change record object to its template
-// 5. Handles event binding/unbinding by implementing the sudo.extensions.listener
-//		extension object
-//
-// `constructor`
-sudo.DataView = function(el, data) {
-  sudo.View.call(this, el, data);
-  // implements the listener extension
-  $.extend(this, sudo.extensions.listener);
-  // renderOnModelChange types observe a data.model
-  if(data.renderOnModelChange) {
-    // will error if no model || the model is not observable
-    this.observer = data.model.observe(this.render.bind(this));
-  }
-};
-// `private`
-sudo.inherit(sudo.View, sudo.DataView);
-// ###addedToParent
-// Container's will check for the presence of this method and call it if it is present.
-// Options affecting this method are: 
-// `renderOnModelChange`: render not called until an observed model is changed via
-// a `set`, `sets` or `unsets` operation.
-// `renderOnAddedToParent`: render is called from this method.
-// If neither is set in this view's model it is up to the developer to call render().
-// Regardless of the rendering options any events in the `event(s)` property are bound
-//
-// `param` {object} `parent` this view's parent
-// `returns` {object} `this`
-sudo.DataView.prototype.addedToParent = function() {
-  this.bindEvents();
-  if(this.data.renderOnAddedToParent) return this.render();
-  return this;
-};
-// ###removedFromParent
-// Remove this object from the DOM, called from the parent's removeChild method.
-//
-// `returns` {Object} `this`
-sudo.DataView.prototype.removedFromParent = function() {
-  this.unbindEvents();
-  this.el.parentNode && this.el.parentNode.removeChild(this.el);
-  // a render on model change type
-  if(this.observer) {
-    this.data.model && this.data.model.unobserve(this.observer);
-    delete this.observer;
-  }
-  return this;
-};
-// ###render
-// (Re)hydrate the innerHTML of this object via its template and data in the `change.object` or `this.data`.
-// If a `renderTarget` is present this Object will inject itself into the target via
-// `this.data.renderMethod` or defualt to `appendChild`. After injection, the `renderTarget`
-// is deleted from this Objects data store (to prevent multiple injection).
-// Event unbinding/rebinding is generally not necessary for the Objects innerHTML as all events from the
-// Object's list of events (`this.data.event(s)`) are delegated to the el when added to parent.
-//
-// `param` {object} `change` dataviews may be observing a model if `renderOnModelChange: true`
-// `returns` {Object} `this`
-sudo.DataView.prototype.render = function(change) {
-  var d = this.data;
-  // (re)hydrate the innerHTML
-  if(typeof d.template === 'string') d.template = sudo.template(d.template);
-  this.el.innerHTML = d.template(change && change.object || d);
-  // am I in the dom yet?
-  if(d.renderTarget) {
-    this._normalizedEl_(d.renderTarget)[d.renderMethod || 'appendChild'](this.el);
-    delete d.renderTarget;
-  }
-  return this;
-};
-// `private`
-sudo.DataView.prototype.role = 'dataview';
 // ##Navigator Class Object
 
 // Abstracts location and history events, parsing their information into a 
@@ -1455,44 +1375,6 @@ sudo.delegates.Change.prototype.filter = function(change) {
 };
 // `private`
 sudo.delegates.Change.prototype.role = 'change';
-//##Data Delegate
-
-// Delegates, if present, can extend the behavior
-// of objects, lessening the need for subclassing. 
-// The data delegate is specifically designed to
-// filter through an object, looking for specified keys or paths
-// and returning values for those if found
-//
-// `param` {Object} data
-// `returns` {*} the value found at the specified key/path if found
-sudo.delegates.Data = function(data) {
-  this.construct(data);
-};
-// inherits from the Filtered Delegate
-sudo.delegates.Data.prototype = Object.create(sudo.delegates.Filtered.prototype);
-// ###filter
-// iterates over a given object literal and returns a value (if present)
-// located at a given key or path
-//
-// `param` {Object} `obj`
-sudo.delegates.Data.prototype.filter = function(obj) {
-  var filters = this.data.filters, sl, o;
-  Object.keys(filters).forEach(function(k) {
-    // keys and paths need different handling
-    if(k.indexOf('.') === -1) {
-      if(k in obj) this.delegator[filters[k]].call(this.delegator, obj[k]);	
-    } else {
-      // the chars after the last refinement are the key we need to check for
-      sl = k.slice(k.lastIndexOf('.') + 1);
-      // and the ones prior are the object
-      o = sudo.getPath(k.slice(0, k.lastIndexOf('.')), obj);
-      if(o && sl in o) this.delegator[filters[k]].call(this.delegator, o[sl]);
-    }
-  }.bind(this));
-};
-// `private`
-sudo.delegates.Data.prototype.role = 'data';
-
 sudo.version = "0.9.9";
 window.sudo = sudo;
 if(typeof window._ === "undefined") window._ = sudo;
