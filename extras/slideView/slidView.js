a1751af909eabcb88fb710d283977b6a83652018
Brendan Saunders <brendan@taskrabbit.com>
2014-05-12 12:24:38 -0700

    in-between

--- extras/slideView/slidView.js
+++ extras/slideView/slidView.js
@@ -4,6 +4,7 @@ _.namespace('lib.views');
 lib.views.slidView = function(el, data) {
   // the slideview will pass the template
   this.construct(el, data);
+  this.transitionEnd = 'webkitTransitionEnd';
 };
 
 lib.views.slidView.prototype = $.extend(Object.create(_.View.prototype), {
@@ -14,22 +15,43 @@ lib.views.slidView.prototype = $.extend(Object.create(_.View.prototype), {
     if(!parent.direction) parent.slides.appendChild(this.el);
     // otherwise do the anim/trans when i am added
     else this[parent.direction === 'left' ? 'inRight': 'inleft']();
+
+    $(this.el).on(this.transitionEnd, function() {
+      debugger;
+    });
   },
-  
+
+  removedFromParent: function removedFromParent(parent) {
+    this.parent.removeChild(this.el);
+  },
+
   inLeft: function inLeft() {
+    //debugger;
+    this.el.classList.add('slide-out-left');
     this.parent.slides.appendChild(this.el);
+    this.el.classList.add('slide-in');
   },
-  
+
   outLeft: function outLeft() {
     // regardless, i remove my DOM
-    this.parent.slides.removeChild(this.el);
+    this.el.classList.remove('slide-in');
+    this.el.classList.remove('slide-out-right');
+    this.el.classList.add('slide-out-left');
+    //debugger;
   },
-  
+
   inRight: function inRight() {
+    debugger;
+    this.el.classList.add('slide-out-right');
     this.parent.slides.appendChild(this.el);
+    this.el.classList.add('slide-in');
   },
-  
+
   outRight: function outRight() {
-    this.parent.slides.removeChild(this.el);
+    this.el.classList.remove('slide-in');
+    this.el.classList.remove('slide-out-left');
+    this.el.classList.add('slide-out-right');
+    //debugger;
+    //$(this.el).on(this.transitionEnd, this.parent.removeChild.bind(this));
   }
 });
--- extras/slideView/slideView.js
+++ extras/slideView/slideView.js
@@ -8,41 +8,40 @@ lib.views.slideView = function(el, data) {
     minX: 10,
     preventDefaults: true
   }, data || {}));
-  
+
   // where the junk happens
   this.slides = this.qs('#slides');
-  
+
+  // because this does not exist in static TODO needed?
+  this.transitionEnd = 'webkitTransitionEnd';
+
   this.bindListeners();
-    
+
   // keep these in a nodeList to pass to the children
   this.childTemplates = document.querySelectorAll('.template');
-  
+
   // which 'slide' is visible
   this.currentSlide = 0;
-  
+
   // the swipable area is initally empty, important so that reverse sliding
   // back to 0 knows how to hydrate
   this.hydrateChild(this.currentSlide);
-
-  // because this does not exist in static TODO needed?
-  this.transitionEnd = '';
-  this.animationEnd = '';
 };
 
 lib.views.slideView.prototype = $.extend(Object.create(_.View.prototype), {
   bindListeners: function bindListeners() {
     if(!('ontouchstart' in document.documentElement)) return;
-    
+
     $(this.slides).on('touchstart', this.touchStarted.bind(this));
   },
-  
+
   // stop listening, we have a swipe
   cancelTouch: function cancelTouch() {
     $(this.slides).off('touchmove');
     this.startX = undefined;
     this.isMoving = false;
   },
-  
+
   dehydrateChild: function dehydrateChild(n, dir) {
     // a child will get removed from the swipable area before a new one is added
     this.visibleChild = null;
@@ -50,7 +49,7 @@ lib.views.slideView.prototype = $.extend(Object.create(_.View.prototype), {
     // now add the next
     this.hydrateChild(this.currentSlide, dir);
   },
-  
+
   hydrateChild: function hydrateChild(n, dir) {
     if(!this.children[n]) {
       // doubt they need a name
@@ -63,10 +62,10 @@ lib.views.slideView.prototype = $.extend(Object.create(_.View.prototype), {
     // the visible child is either the index or the last one added
     this.visibleChild = this.children[n];
   },
-  
+
   swipeLeft: function swipeLeft() {
     this.direction = 'left';
-    // if we are at the last child there is nothing to do. we cannot have 
+    // if we are at the last child there is nothing to do. we cannot have
     // more children than templates
     if(this.currentSlide === (this.childTemplates.length - 1)) return;
     // we are increasing
@@ -75,7 +74,7 @@ lib.views.slideView.prototype = $.extend(Object.create(_.View.prototype), {
     if(this.children[this.currentSlide - 1]) this.dehydrateChild(this.currentSlide - 1, 'left');
     else this.hydrateChild(this.currentSlide, 'left');
   },
-  
+
   swipeRight: function swipeRight() {
     this.direction = 'right';
     // if we are at the first child there is nothing to do.
@@ -90,7 +89,7 @@ lib.views.slideView.prototype = $.extend(Object.create(_.View.prototype), {
   touchMoved: function touchMoved(e) {
     var x, deltaX;
     if(this.data.preventDefaults) e.preventDefault();
-    
+
     if(this.isMoving) {
       x = e.touches[0].pageX;
       deltaX = this.startX - x;
@@ -104,9 +103,14 @@ lib.views.slideView.prototype = $.extend(Object.create(_.View.prototype), {
   touchStarted: function touchStarted(e) {
     // the touches would represent fingers so we only care when it's 1
     if(e.touches.length !== 1) return;
-    
+
     this.startX = e.touches[0].pageX;
     this.isMoving = true;
     $(this.slides).on('touchmove', this.touchMoved.bind(this));
+  },
+
+  removeChild: function removeChild(child) {
+    debugger;
+    this.base('removeChild', child);
   }
 });
